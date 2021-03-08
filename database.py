7a06ee3cc3316fd0d34ba4b1eccb3ea08781374e
nefedov-it <y.nefedov.work@gmail.com>
2021-03-08 21:39:26 +0300

    Wrote database.py, added description to keyboard.py
    Wrote database.py, added description to keyboard.py

--- database.py
+++ database.py
@@ -1,38 +1,96 @@
 import sqlite3
 
+
 class Database:
     """Класс, отвечающий за базу данных"""
+
     def __init__(self, path_to_db):
-        path_to_db = path_to_db
+        self.path_to_db = path_to_db
 
     def connection(self):
         """Подключение базы данных"""
-        pass
-    def excecute(self):
+        return sqlite3.connect(self.path_to_db)
 
-        pass
+    def execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
+
+        if not parameters:
+            parameters = ()
+
+        connection = self.connection()
+        connection.set_trace_callback(logger)
+        cursor = connection.cursor()
+        data = None
+        cursor.execute(sql, parameters)
+
+        if commit:
+            connection.commit()
+        if fetchall:
+            data = cursor.fetchall()
+        if fetchone:
+            data = cursor.fetchone()
+
+        connection.close()
+        return data
+
     def create_table(self):
         """Создание таблицы: id, fullname(tg), phone_number"""
         """В дальнейшем необходимо добавить в таблицу желание записаться на курс/интенсив"""
-        pass
-    def format_args(self):
-        """Форматирование аргументов, для удобного составления запроса в бд"""
-        pass
-    def add_user(self):
+        sql = """
+        CREATE TABLE Users (
+        id int NOT NULL,
+        fullname varchar(255) NOT NULL, 
+        phone varchar(12),
+        PRIMARY KEY (id)
+        )"""
+
+        self.execute(sql, commit=True)
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        """Форматирование аргументов, для удобного составления запроса в БД"""
+        sql += " AND ".join([
+            f"{item} = ?" for item in parameters
+        ])
+
+        return sql, tuple(parameters.values())
+
+    def add_user(self, id: int, fullname: str, phone: str = None):
         """Добавление пользователя в БД"""
-        pass
-    def select_user(self):
-        """Выбор пользователя из БД"""
-        pass
-    def select_all_user(self):
+        # SQL_EXAMPLE = "INSERT INTO Users(id, Name, email) VALUES(1, 'John', 'John@gmail.com')"
+        sql = """
+        INSERT INTO Users(id, fullname, phone) VALUES (?, ?, ?)
+        """
+        self.execute(sql, parameters=(id, fullname, phone), commit=True)
+
+    def select_all_users(self):
         """Выбор всех пользователей"""
-        pass
-    def update_user_phone_number(self):
+        sql = """SELECT FROM * Users"""
+        return self.execute(sql, fetchall=True)
+
+    def select_user(self, **kwargs):
+        """Выбор пользователя из БД"""
+        sql = "SELECT * FROM Users WHERE "
+        sql, parameters = self.format_args(sql, kwargs)
+
+        return self.execute(sql, parameters=parameters, fetchone=True)
+
+    def count_users(self):
+        return self.execute("SELECT COUNT(*) FROM Users;", fetchone=True)
+
+    def update_user_phone(self, phone, id):
         """Добавление номера телефона в БД"""
-        pass
+        # SQL_EXAMPLE = "UPDATE Users SET phone=+79876543210 WHERE id=12345"
+
+        sql = f"""
+        UPDATE Users SET phone=? WHERE id=?
+        """
+
+        return self.execute(sql, parameters=(phone, id), commit=True)
+
     def delete_user(self):
         """Удаление пользователя из БД"""
         pass
+
 
 def logger(statement):
     print(f"""
--- keyboard.py
+++ keyboard.py
@@ -1,11 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
 
+"""Основная клавиатура с предложением услуг"""
 check_list_kb = ReplyKeyboardMarkup(resize_keyboard=True,
                                     keyboard=[
                                         [
                                             KeyboardButton(text="Хочу чек-лист")
                                         ]
                                     ])
+
+"""Вспомогательная клавиатура с кнопокй, запрашивающей номер телефона"""
 phone_kb = ReplyKeyboardMarkup(resize_keyboard=True,
                                keyboard=[
                                    [
